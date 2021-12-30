5bd4c06f9630454109cfea331c8993220a944ef4
Manibharathi <bharathi.mani@inxsasia.com>
2021-12-30 12:26:14 +0530

    first commit

---
+++ lib/main_tst.dart
@@ -0,0 +1,10 @@
+import 'dart:async';
+
+import 'package:flavours_app/app_config.dart';
+import 'package:flavours_app/main_common.dart';
+
+Future<void> main() async {
+  final config = AppConfig('tst');
+
+  mainCommon(config);
+}
